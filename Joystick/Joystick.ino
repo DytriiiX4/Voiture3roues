f548af4ab5154f075bd5e4d07364e181d93df39b
DytriiiX4 <147625602+DytriiiX4@users.noreply.github.com>
2025-12-16 10:40:39 +0100

    Update du Joystick

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,12 +1,26 @@
-button = 2;
-
+int button = 2;
+int xAxis = A0;
+int yAxis = A1;
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup ode here, to run once:
+  pinMode(button, INPUT_PULLUP);
+  pinMode(xAxis, INPUT);
+  pinMode(yAxis, INPUT);
 
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int buttonPressed = digitalRead(button);
+  int xAxisPos = analogRead(xAxis);
+  int yAxisPos = analogRead(yAxis);
+  Serial.println("Here're all the info u need.");
+  Serial.print("Button Status : ");
+  Serial.println(buttonPressed);
+  Serial.print("Axis X Status : ");
+  Serial.println(xAxisPos);
+  Serial.print("Axis Y Status : ");
+  Serial.println(yAxisPos);
+  delay(250);
 }
