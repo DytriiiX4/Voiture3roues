9fa9a00ac1fcdd96d436182e15252515d961f1a2
DytriiiX4 <147625602+DytriiiX4@users.noreply.github.com>
2025-12-18 19:04:18 +0100

    Bluetooth pour contrôler la voiture fini mais pas test

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,25 +1,11 @@
+String sending;
+
 // Broches du joystick
 int button = 2;
 int xAxis = A0;
 int yAxis = A1;
 
-// Broches du pont en H
-int in1 = 3;
-int in2 = 4;
-int ena = 5;
-int enb = 6;
-int in3 = 7;
-int in4 = 8;
 char state = 's';
-
-void stop()  {
-  digitalWrite(in1, 0);
-  digitalWrite(in2, 0);
-  digitalWrite(in3, 0);
-  digitalWrite(in4, 0);
-  digitalWrite(ena, 0);
-  digitalWrite(enb, 0);
-}
 
 void setup() {
   // Joystick
@@ -27,104 +13,50 @@ void setup() {
   pinMode(xAxis, INPUT);
   pinMode(yAxis, INPUT);
 
-  // Pont en H
-  for (int i = 3; i < 9; i++) {
-    pinMode(i, OUTPUT);
-  }
-
   Serial.begin(9600);
-
-  // Temps d'initialisation
-  stop();
-  Serial.println("stop");
   delay(5000);
 }
 
-void avancer()  {
-  digitalWrite(in1, 0);
-  digitalWrite(in2, 1);
-  digitalWrite(in3, 1);
-  digitalWrite(in4, 0);
-
+void enableForwardBackward(long xPos, long yPos) {
+  long absol = abs(516 - xPos);
+  int leftWheelSpeed = (((absol * 100) / 412) + ((yPos * 100) / 825));
+  int rightWheelSpeed = (((absol * 100) / 412) + (((1023 - yPos) * 100) / 825));
+  sending += '.';
+  sending += leftWheelSpeed;
+  sending += '.';
+  sending += rightWheelSpeed;
 }
 
-void reculer()  {
-  digitalWrite(in1, 1);
-  digitalWrite(in2, 0);
-  digitalWrite(in3, 0);
-  digitalWrite(in4, 1);
-}
-
-void droite()  {
-  digitalWrite(in1, 0);
-  digitalWrite(in2, 1);
-  digitalWrite(in3, 0);
-  digitalWrite(in4, 1);
-}
-
-void gauche()  {
-  digitalWrite(in1, 1);
-  digitalWrite(in2, 0);
-  digitalWrite(in3, 1);
-  digitalWrite(in4, 0);
-}
-
-void enableLeftRight(long yPos)  {
-  long absol = abs(516-yPos);
-  int bothWheelSpeed = (absol*100)/206;
-  Serial.println(bothWheelSpeed);
-  Serial.println(bothWheelSpeed);
-  analogWrite(ena, bothWheelSpeed);
-  analogWrite(enb, bothWheelSpeed);
-  Serial.println(" ");
-}
-
-void enableForwardBackward(long xPos, long yPos)  {
-  long absol = abs(516-xPos);
-  int leftWheelSpeed = (((absol*100)/412)+((yPos*100)/825));
-  int rightWheelSpeed =  (((absol*100)/412)+(((1023-yPos)*100)/825));
-  analogWrite(enb, leftWheelSpeed);
-  analogWrite(ena, rightWheelSpeed);
-  // Serial.println(leftWheelSpeed);
-  // Serial.println(rightWheelSpeed);
-  // Serial.println(" ");
+void enableLeftRight(long yPos) {
+  long absol = abs(516 - yPos);
+  int bothWheelSpeed = (absol * 100) / 206;
+  sending += '.';
+  sending += bothWheelSpeed;
 }
 
 void loop() {
-  int buttonPressed = digitalRead(button);
   long xAxisPos = analogRead(xAxis);
   long yAxisPos = analogRead(yAxis);
-  // Serial.println("Here're all the info u need.");
-  // Serial.print("Button Status : ");
-  // Serial.println(buttonPressed);
-  // Serial.print("Axis X Status : ");
-  // Serial.println(xAxisPos);
-  // Serial.print("Axis Y Status : ");
-  // Serial.println(yAxisPos);
+
   if (xAxisPos < 500) {
-    // Serial.println("avancer");
-    avancer();
+    sending += 'a';
     enableForwardBackward(xAxisPos, yAxisPos);
-  }
-  else if(xAxisPos > 530) {
-    // Serial.println("reculer");
-    reculer();
+  } else if (xAxisPos > 530) {
+    sending += 'r';
     enableForwardBackward(xAxisPos, yAxisPos);
-  }
-  else{
-    if (yAxisPos < 510){
-      // Serial.println("gauche");
-      gauche();
+  } else {
+    if (yAxisPos < 510) {
+      sending += 'g';
       enableLeftRight(yAxisPos);
-    }
-    else if (yAxisPos > 530) {
-      // Serial.println("droite");
-      droite();
+    } else if (yAxisPos > 530) {
+      sending += 'd';
       enableLeftRight(yAxisPos);
-    }
-    else  {
-      // Serial.println("stop");
-      stop();
+    } else {
+      sending += 's';
     }
   }
+  sending += "\n";
+  Serial.print(sending);
+  sending = "";
+  delay(5000);
 }
--- Pont_en_H/pont_en_h/pont_en_h.ino
+++ Pont_en_H/pont_en_h/pont_en_h.ino
@@ -1,22 +1,132 @@
+char infos;
+
+int index = 0;
+
+String speed1str;
+String speed2str;
+
+char state;
+int speed1;
+int speed2;
+
 int in1 = 3;
 int in2 = 4;
-int in3 = 5;
-int in4 = 6;
+int ena = 5;
+int enb = 6;
+int in3 = 7;
+int in4 = 8;
+
+void stop() {
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 0);
+  digitalWrite(in3, 0);
+  digitalWrite(in4, 0);
+  digitalWrite(ena, 0);
+  digitalWrite(enb, 0);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  for (int i = 3; i < 7; i++) {
+  for (int i = 3; i < 9; i++) {
     pinMode(i, OUTPUT);
   }
 
+  stop();
   Serial.begin(9600);
+  delay(5000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  //avant
+void avancer() {
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 1);
+  digitalWrite(in3, 1);
+  digitalWrite(in4, 0);
+}
+
+void reculer() {
+  digitalWrite(in1, 1);
+  digitalWrite(in2, 0);
+  digitalWrite(in3, 0);
+  digitalWrite(in4, 1);
+}
+
+void droite() {
   digitalWrite(in1, 0);
   digitalWrite(in2, 1);
   digitalWrite(in3, 0);
   digitalWrite(in4, 1);
+}
+
+void gauche() {
+  digitalWrite(in1, 1);
+  digitalWrite(in2, 0);
+  digitalWrite(in3, 1);
+  digitalWrite(in4, 0);
+}
+
+void enableLeftRight(int bothWheelSpeed) {
+  analogWrite(ena, bothWheelSpeed);
+  analogWrite(enb, bothWheelSpeed);
+}
+
+void enableForwardBackward(int leftWheelSpeed, int rightWheelSpeed) {
+  analogWrite(enb, leftWheelSpeed);
+  analogWrite(ena, rightWheelSpeed);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  index = 0;
+  while (infos != '\n') {
+    if (Serial.available())  {
+      infos = Serial.read();
+      Serial.print(infos);
+      
+      if (infos == '.') {
+        index++;
+      }
+        else if (infos == '\n')  {
+          index = 0;
+          Serial.print(state);
+          Serial.print(speed1str);
+          Serial.print(speed2str);
+        }
+      else{
+        if (index == 0) {
+          state = infos;
+        }
+        else if (index == 1)  {
+          speed1str += infos;
+        }
+        else {
+          speed2str += infos;
+        }
+      }
+    }
+  }
+
+  infos = '\0';
+  speed1 = speed1str.toInt();
+  speed2 = speed2str.toInt();
+
+  if (state == 'a') {
+    avancer();
+    enableForwardBackward(speed1, speed2);
+  }
+  else if (state == 'r')  {
+    reculer();
+    enableForwardBackward(speed1, speed2);
+  }
+  else if (state == 'g')  {
+    gauche();
+    enableLeftRight(speed1);
+  }
+  else if (state == 'd')  {
+    droite();
+    enableLeftRight(speed1);
+  }
+  else {  stop();  }
+
+  speed1str = "";
+  speed2str = "";
 }
